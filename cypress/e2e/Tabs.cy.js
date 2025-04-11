cea655a3164a0caedeb1b70e6be92dab8d31bd39
AnuradhaLiyanage <it20225438@my.sliit.lk>
2025-04-11 13:30:07 +0530

    Cypress E2E Web Automation | Interacting with Elements | Handling Child Tabs - updated

--- cypress/e2e/Tabs.cy.js
+++ cypress/e2e/Tabs.cy.js
@@ -12,4 +12,22 @@ describe('Handle tabs', ()=>{
         //operations - go back to parent window
         cy.go('back');
     })
+
+    it.only("Approaach 2", ()=>{
+        cy.visit('https://the-internet.herokuapp.com/windows'); //open parent tab
+        //cy.get('.example>a').click(); // we can use like this way, but we can't do anthing in new window
+
+        cy.get('.example>a').then((e)=>{
+            let url=e.prop("href");
+            cy.visit(url);
+        })
+
+        //check url in new window
+        cy.url().should('include','https://the-internet.herokuapp.com/windows/new')
+
+        cy.wait(3000);
+        //operations - go back to parent window
+        cy.go('back');
+        
+    })
 })
