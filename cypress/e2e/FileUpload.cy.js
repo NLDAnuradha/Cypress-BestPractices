a3e227580867f20b4cf02f342c0a13c9b04a860e
AnuradhaLiyanage <it20225438@my.sliit.lk>
2025-06-05 02:05:35 +0530

    Updated FileUpload.cy.js file

--- cypress/e2e/FileUpload.cy.js
+++ cypress/e2e/FileUpload.cy.js
@@ -2,7 +2,7 @@ import 'cypress-file-upload';
 
 describe('File Upload', (()=>{
 
-    it.skip('Single File Upload', ()=>{
+    it('Single File Upload', ()=>{
         cy.visit('https://testpages.herokuapp.com/styled/file-upload-test.html'); //visit the website
         cy.get('#fileinput').attachFile('Test1.pdf'); // upload the file
         cy.get('input[type="submit"][value="Upload"]').click();
@@ -14,7 +14,7 @@ describe('File Upload', (()=>{
         
     })
 
-    it.skip('File Upload - Rename', ()=>{
+    it('File Upload - Rename', ()=>{
         cy.visit('https://testpages.herokuapp.com/styled/file-upload-test.html'); //visit the website
         cy.get('#fileinput').attachFile({filePath:'Test1.pdf',fileName:'myFile.pdf'}); // upload the file
         cy.get('input[type="submit"][value="Upload"]').click();
@@ -25,21 +25,30 @@ describe('File Upload', (()=>{
         cy.get('p').should('contain', 'You uploaded');
     })
 
-    it.only('File Upload - Drag and Drop', ()=>{
+    it('File Upload - Drag and Drop', ()=>{
         cy.visit('https://www.dragdrop.com/test/'); //visit the website
         cy.get('#demo-upload').attachFile('Test1.pdf', {subjectType:'drag-n-drop'});
-        //cy.get('#file-submit').click();
+        
         cy.wait(3000);
+
         //verify the upload succes message
         cy.get('.dz-filename > span').should('contain', 'Test1.pdf');
         
     })
 
-    it('Multiple File Upload', ()=>{
+    it('Multiple File Upload', ()=>{ 
+        cy.visit('https://davidwalsh.name/demo/multiple-file-upload.php'); //visit the website
+        cy.get('#filesToUpload').attachFile(['Test1.pdf', 'Test2.pdf']); // upload the file
         
+        cy.wait(3000);
+        
+        cy.get(':nth-child(6) > strong').should('contain.text', 'Files You Selected:');
     })
 
-    it('File Upload - Shadow Dom', ()=>{
-        
+    it.only('File Upload - Shadow Dom', ()=>{
+         cy.visit("https://www.htmlelements.com/demos/fileupload/shadow-dom/index.htm");
+         cy.get(".smart-browse-input", {includeShadowDom:true}).attachFile('Test1.pdf');
+         cy.wait(3000);
+         cy.get(".smart-item-name", {includeShadowDom:true}).contains("Test1.pdf");  
     })
 }))
