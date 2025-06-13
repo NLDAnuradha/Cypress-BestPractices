49f62492c3f2f9c564afa3d17484f895fb79eb18
AnuradhaLiyanage <it20225438@my.sliit.lk>
2025-06-13 16:22:40 +0530

    Updated

--- cypress/e2e/Screenshots&Videos.cy.js
+++ cypress/e2e/Screenshots&Videos.cy.js
@@ -1,13 +1,22 @@
 describe('TestSuite', ()=>{
 
     it('Capture Screenshots and Videos', ()=>{
-        //capture as we wish
+        
         cy.visit('https://automationteststore.com/');
+
+        //capture entire page
         cy.screenshot("Homepage");
 
         cy.wait(5000);
-        //specifi element
+
+        //capture specific element 
         cy.get(".row.promo_section").screenshot("FirstElement")
-        // captuer if we got failed
+        
+        
+        //Automatically capture scrnshot & video on failure (only happens when execute through CLI)
+        cy.get("ul[id='customer_menu_top'] li a").click();
+        cy.get("div[class='col-sm-6 newcustomer'] h2[class='heading2']").should('have.text', "Account Login.");
+        
+
     })
 })
